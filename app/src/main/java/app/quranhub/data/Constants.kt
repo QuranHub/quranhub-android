fd7cb780ec7ddf51cc80506daee337de350319f3
Abdallah Abdelazim <abdallah.abdelazim@hotmail.com>
2023-05-08 02:10:17 +0300

    Convert Constants to Kotlin

--- app/src/main/java/app/quranhub/data/Constants.kt
+++ app/src/main/java/app/quranhub/data/Constants.kt
@@ -1,103 +1,99 @@
-package app.quranhub.data;
+package app.quranhub.data
 
-import java.io.File;
-import java.util.Arrays;
-import java.util.List;
+import app.quranhub.R
+import java.io.File
 
-import app.quranhub.R;
+object Constants {
+    const val API_BASE_URL = "https://api.quranhub.app"
+    const val STATIC_FILES_BASE_URL = "https://www.quranhub.app"
 
-public final class Constants {
-
-    private Constants() {
-        // Prevent instantiation
-    }
-
-    public static final String API_BASE_URL = "https://api.quranhub.app";
-    public static final String STATIC_FILES_BASE_URL = "https://www.quranhub.app";
-
-    public static final class Quran {
-
-        private Quran() {
-        }
-
-        public static final String HAFS_IMG_BASE_URL =
-                STATIC_FILES_BASE_URL + "/quran-pages-images/kfgqpc/hafs-wasat/";
-        public static final String WARSH_IMG_BASE_URL =
-                STATIC_FILES_BASE_URL + "/quran-pages-images/kfgqpc/warsh/";
-
-        public static final int NUM_OF_PAGES = 604;
+    object Quran {
+        const val HAFS_IMG_BASE_URL = "$STATIC_FILES_BASE_URL/quran-pages-images/kfgqpc/hafs-wasat/"
+        const val WARSH_IMG_BASE_URL = "$STATIC_FILES_BASE_URL/quran-pages-images/kfgqpc/warsh/"
+        const val NUM_OF_PAGES = 604
 
         // Quran pages sizes in pixels
-        public static final int HAFS_PAGE_IMG_ORIGINAL_WIDTH = 807;
-        public static final int HAFS_PAGE_IMG_ORIGINAL_HEIGHT = 1205;
-        public static final int HAFS_PAGE_IMG_FIRST_TWO_ORIGINAL_WIDTH = 843;
-        public static final int HAFS_PAGE_IMG_FIRST_TWO_ORIGINAL_HEIGHT = 1140;
-
-        public static final int WARSH_PAGE_IMG_ORIGINAL_WIDTH = 1064;
-        public static final int WARSH_PAGE_IMG_ORIGINAL_HEIGHT = 1570;
-        public static final int WARSH_PAGE_IMG_FIRST_TWO_ORIGINAL_WIDTH = 1211;
-        public static final int WARSH_PAGE_IMG_FIRST_TWO_ORIGINAL_HEIGHT = 1640;
-
-        public static final int NUM_OF_VERSES = 6236;
+        const val HAFS_PAGE_IMG_ORIGINAL_WIDTH = 807
+        const val HAFS_PAGE_IMG_ORIGINAL_HEIGHT = 1205
+        const val HAFS_PAGE_IMG_FIRST_TWO_ORIGINAL_WIDTH = 843
+        const val HAFS_PAGE_IMG_FIRST_TWO_ORIGINAL_HEIGHT = 1140
+        const val WARSH_PAGE_IMG_ORIGINAL_WIDTH = 1064
+        const val WARSH_PAGE_IMG_ORIGINAL_HEIGHT = 1570
+        const val WARSH_PAGE_IMG_FIRST_TWO_ORIGINAL_WIDTH = 1211
+        const val WARSH_PAGE_IMG_FIRST_TWO_ORIGINAL_HEIGHT = 1640
+        const val NUM_OF_VERSES = 6236
     }
 
-    public static final class BookmarkType {
-
-        private BookmarkType() {
-        }
-
-        public static final int FAVORITE = 1;
-        public static final int RECITING = 2;
-        public static final int NOTE = 3;
-        public static final int MEMORIZE = 4;
+    object BookmarkType {
+        const val FAVORITE = 1
+        const val RECITING = 2
+        const val NOTE = 3
+        const val MEMORIZE = 4
     }
 
-    public static final class Directory {
+    object Directory {
+        const val ROOT_PUBLIC = "QuranHub"
 
-        private Directory() {
-        }
-
-        public static final String ROOT_PUBLIC = "QuranHub";
-        public static final String LIBRARY_PUBLIC = ROOT_PUBLIC + File.separator + "Library";
-
-        public static final String NOTE_VOICE_RECORDER = "Note_Recorder";
-        public static final String AYA_VOICE_RECORDER = "Aya_Recorder";
-        public static final String QURAN_AUDIO = ".quran_audio";
+        @JvmField
+        val LIBRARY_PUBLIC = ROOT_PUBLIC + File.separator + "Library"
+        const val NOTE_VOICE_RECORDER = "Note_Recorder"
+        const val AYA_VOICE_RECORDER = "Aya_Recorder"
+        const val QURAN_AUDIO = ".quran_audio"
     }
 
-    public static final class Language {
-
-        private Language() {
-        }
-
-        public static final String ENGLISH_CODE = "en";
-        public static final String ARABIC_CODE = "ar";
-        public static final String SPANISH_CODE = "es";
-        public static final String FRENCH_CODE = "fr";
-        public static final String HAUSA_CODE = "ha";
-        public static final String INDONESIAN_CODE = "in";
-        public static final String URDU_CODE = "ur";
-
-        public static final String DEFAULT_APP_LANGUAGE = ENGLISH_CODE;
+    object Language {
+        const val ENGLISH_CODE = "en"
+        const val ARABIC_CODE = "ar"
+        const val SPANISH_CODE = "es"
+        const val FRENCH_CODE = "fr"
+        const val HAUSA_CODE = "ha"
+        const val INDONESIAN_CODE = "in"
+        const val URDU_CODE = "ur"
+        const val DEFAULT_APP_LANGUAGE = ENGLISH_CODE
 
         /* It's important that the indices of languages is the same in CODES, NAMES_STR_IDS & FLAGS_DRAWABLE_IDS */
-        public static final List<String> CODES = Arrays.asList(ENGLISH_CODE, ARABIC_CODE, SPANISH_CODE, FRENCH_CODE
-                , HAUSA_CODE, INDONESIAN_CODE, URDU_CODE);
-        public static final int[] NAMES_STR_IDS = {R.string.english_language, R.string.arabic_language, R.string.spanish_language
-                , R.string.french_language, R.string.hausa_language, R.string.indonesian_language, R.string.urdu_language};
-        public static final int[] FLAGS_DRAWABLE_IDS = {R.drawable.flag_en, R.drawable.flag_ar, R.drawable.flag_es
-                , R.drawable.flag_fr, R.drawable.flag_ha, R.drawable.flag_in, R.drawable.flag_ur};
+        @JvmField
+        val CODES = listOf(
+            ENGLISH_CODE,
+            ARABIC_CODE,
+            SPANISH_CODE,
+            FRENCH_CODE,
+            HAUSA_CODE,
+            INDONESIAN_CODE,
+            URDU_CODE
+        )
+
+        @JvmField
+        val NAMES_STR_IDS = intArrayOf(
+            R.string.english_language,
+            R.string.arabic_language,
+            R.string.spanish_language,
+            R.string.french_language,
+            R.string.hausa_language,
+            R.string.indonesian_language,
+            R.string.urdu_language
+        )
+
+        @JvmField
+        val FLAGS_DRAWABLE_IDS = intArrayOf(
+            R.drawable.flag_en,
+            R.drawable.flag_ar,
+            R.drawable.flag_es,
+            R.drawable.flag_fr,
+            R.drawable.flag_ha,
+            R.drawable.flag_in,
+            R.drawable.flag_ur
+        )
     }
 
-    public static final class Recitation {
-        public static final String HAFS_KEY = "hafs";
-        public static final String WARSH_KEY = "warsh";
-
-        public static final int HAFS_ID = 0;
-        public static final int WARSH_ID = 1;
+    object Recitation {
+        const val HAFS_KEY = "hafs"
+        const val WARSH_KEY = "warsh"
+        const val HAFS_ID = 0
+        const val WARSH_ID = 1
 
         /* It's important that the index of any recitation name is the same as the ID integer given for it above */
-        public static final int[] NAMES_STR_IDS = {R.string.hafs_recitation, R.string.warsh_recitation};
+        @JvmField
+        val NAMES_STR_IDS = intArrayOf(R.string.hafs_recitation, R.string.warsh_recitation)
     }
-
 }
