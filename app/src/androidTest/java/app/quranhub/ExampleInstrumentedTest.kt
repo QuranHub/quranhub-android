1f4e072448eb38811147d7e40476876fe7de18f0
Abdallah Abdelazim <abdallah.abdelazim@hotmail.com>
2023-05-11 20:38:56 +0300

    Convert Java to Kotlin

--- app/src/androidTest/java/app/quranhub/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/app/quranhub/ExampleInstrumentedTest.kt
@@ -1,27 +1,24 @@
-package app.quranhub;
+package app.quranhub
 
-import static org.junit.Assert.assertEquals;
-
-import android.content.Context;
-
-import androidx.test.core.app.ApplicationProvider;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @see [Testing documentation](http://d.android.com/tools/testing)
  */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = ApplicationProvider.getApplicationContext();
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
 
-        assertEquals("app.quranhub", appContext.getPackageName());
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = ApplicationProvider.getApplicationContext<Context>()
+        Assert.assertEquals("app.quranhub", appContext.packageName)
     }
 }
