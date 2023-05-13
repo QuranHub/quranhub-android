6ec20308881fff6f058b99493f2431de53baa4a0
Abdallah Abdelazim <abdallah.abdelazim@hotmail.com>
2023-05-13 20:35:01 +0300

    Add Quran audio for Ibrahim Al-Dosary

--- app/src/main/java/app/quranhub/util/QuranAudioDownloadUtils.kt
+++ app/src/main/java/app/quranhub/util/QuranAudioDownloadUtils.kt
@@ -29,6 +29,8 @@ object QuranAudioDownloadUtils {
             sb.append("quran-audio-hafs-sudais")
         } else if (sheikhId == "yassin_aljazaery" && recitationId == Constants.Recitation.WARSH_ID) {
             sb.append("quran-audio-warsh-aljazaery")
+        } else if (sheikhId == "ibrahim_aldosary" && recitationId == Constants.Recitation.WARSH_ID) {
+            sb.append("quran-audio-warsh-ibrahim-aldosary")
         }
         sb.append("/verses/")
 
