543fe223de126d72ed45394bb03dc892a11cbfe8
Abdallah Abdelazim <abdallah.abdelazim@hotmail.com>
2024-07-04 00:16:31 +0300

    Migrate to Gradle Kotlin DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext.kotlin_version = '2.0.0'
+    val kotlin_version by extra("2.0.0")
     repositories {
         google()
         mavenCentral()
-
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:8.5.0'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
-        classpath 'com.google.gms:google-services:4.4.2'
-        classpath 'com.google.firebase:firebase-crashlytics-gradle:3.0.2'
-        classpath 'com.google.firebase:perf-plugin:1.4.2'
+        classpath("com.android.tools.build:gradle:8.5.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        classpath("com.google.gms:google-services:4.4.2")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:3.0.2")
+        classpath("com.google.firebase:perf-plugin:1.4.2")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -24,10 +23,9 @@ allprojects {
         google()
         mavenCentral()
         jcenter()
-
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
