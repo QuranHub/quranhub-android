6935ee5e203c4bb5090c975e4526ca708a34dfbf
Abdallah-Abdelazim <abdallah.abdelazim@hotmail.com>
2026-09-12 01:51:55 +0000

    chore(release): 1.8.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "app.quranhub"
         minSdk = libs.versions.minSdkVersion.get().toInt()
         targetSdk = libs.versions.targetSdkVersion.get().toInt()
-        versionCode = 17
-        versionName = "1.8.0"
+        versionCode = 18
+        versionName = "1.8.1"
 
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
